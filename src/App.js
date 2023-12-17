23403500c89baffa455fa0f5b0832aa2b2aae0e0
desaianiruddh <aniruddh.d@simformsolutions.com>
2023-12-17 12:50:24 +0530

    added scroll button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Page1 from './Pages/Page1';
 import './App.css';
 import Page2 from './Pages/Page2';
 import Page3 from './Pages/Page3';
+import { Button } from 'antd';
 
 const App = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [scroll, setScroll] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setScroll(scrollY);
+      setIsVisible(scrollY < window.innerHeight * 2);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  const scrollDown = () => {
+    window.scrollTo({
+      top: scroll + window.innerHeight,
+      behavior: 'smooth',
+    });
+  };
   return (
     <div className="App">
       {/* page 1 */}
@@ -25,6 +48,16 @@ const App = () => {
       <div className="content">
         <Page3 />
       </div>
+      {/* Scroll Button */}
+      <Button
+        onClick={scrollDown}
+        className="next-page-btn"
+        style={{
+          display: isVisible ? 'block' : 'none',
+        }}
+      >
+        Next Page
+      </Button>
     </div>
   );
 };
